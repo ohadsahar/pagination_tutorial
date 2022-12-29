46123dd64c38224916789a9bbc7b402cf30bac1d
Ohad Green <osahar5@gmail.com>
2022-12-29 14:04:24 +0200

    initial commit

--- app/Posts/PostsList/index.tsx
+++ app/Posts/PostsList/index.tsx
@@ -4,9 +4,9 @@ import { PostProps, Props } from '..';
 export const PostsList = ({ posts }: PostProps) => (
   <ul className='flex flex-col h-[calc(100vh-80px)] overflow-y-scroll space-y-5 p-4'>
     {posts.map(({ id, title, body }: Props) => (
-      <li key={id}>
-        #{id} {title}
-        <p>{body}</p>
+      <li key={id} className='shadow-lg p-4 hover:scale-95 transition-all'>
+        # <span className='font-medium'>{id}</span> {title}
+        <p className='font-semibold'>{body}</p>
       </li>
     ))}
   </ul>
